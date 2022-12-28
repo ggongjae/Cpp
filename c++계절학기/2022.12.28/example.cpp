40598e00e27b6a10a597eb0ef67dc5bbc32dc369
ggongjae <cjdak0907@naver.com>
2022-12-28 12:26:22 +0900

    yongjae

--- c++계절학기/2022.12.28/example.cpp
+++ c++계절학기/2022.12.28/example.cpp
@@ -1,15 +1,21 @@
-#include <iostream>
-#include <stdio.h>
-
-using namespace std;
-
-int main(){
-    int num;
-
-    cout << "정수 1개를 입력하시오.";
-    cin >> num;
-    printf("Hello World\n");
-    cout << num << "\n";
-
-    return 0;
-}
+//#include <stdio.h>
+//char msg [] = "존재하지 않는 스트립입니다.";
+//
+//void mystrprint(char *s){
+//    //예외 조건 비교와 throw 작성
+//    if(s == 0 )throw msg;
+//    printf("스트링은: %s 입니다.\n",s);
+//}
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    char *s1 = "Hello";
+//    char *s2 = 0;
+//    try{
+//        mystrprint(s1);
+//        mystrprint(s2);
+//    }catch(const char *s){
+//        cout << s << endl;
+//    }
+//    return 0;
+//}
---
+++ c++계절학기/2022.12.28/rect.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//using namespace std;
+//class Rect{
+//public: //객체변수 instance
+//    int m_width;
+//    int m_height;
+//    int area(){
+//        return m_height * m_width;
+//    }
+//};
+//int main(){
+//    Rect r;
+//    r.m_width = 5; r.m_height = 6;
+//    cout << "면적 " << r.area() << endl;
+//}
