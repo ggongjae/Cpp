bf22229e522b901dbb428f2fb36d7b981e30f3f4
ggongjae <cjdak0907@naver.com>
2022-12-29 12:33:21 +0900

    yongjae

---
+++ c++계절학기/2022.12.29/card/cardm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "card1.h"
+using namespace std;
+
+Card::Card(){
+    suit=0; rank=0;
+}
+int Card::getSuit() { return suit; }
+int Card::getRank() { return  rank; }
+void Card::setSuitRank(int s, int r){
+    if(s >= 0 && s <= 3){
+        suit = s;
+    }
+    if(r >= 0 && r <= 12){
+        rank = r;
+    }
+}
+void Card::flip(){ faceUp = !faceUp; }
+void Card::show() {
+    cout << "Suit " << suit << endl << "Rank " << rank << endl;
+}
+
---
+++ c++계절학기/2022.12.29/init.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Rect{
+//public:
+//    int m_width;
+//    int m_height;
+//    void show(){
+//        cout << "면적 " << m_width * m_height << endl;
+//    }
+//    Rect(){
+//        m_width =6; m_height=5;
+//    }
+//};
+//int main(){
+//    Rect *rect;
+//    rect = new Rect;
+//    rect->show();
+//}
