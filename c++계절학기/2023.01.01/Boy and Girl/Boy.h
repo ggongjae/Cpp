0b8c0946e2e7d9805945c908e04a8e3a6ec736dc
ggongjae <cjdak0907@naver.com>
2023-01-03 12:20:21 +0900

    yongjae

---
+++ c++계절학기/2023.01.01/Boy and Girl/Boy.h
@@ -0,0 +1,18 @@
+//#ifndef UNTITLED2_BOY_H
+//#define UNTITLED2_BOY_H
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//class Girl;
+//class Boy {
+//public:
+//    string m_boyname;
+//    Girl *m_girlfriend;
+//    Boy(string bname):m_boyname(bname), m_girlfriend(0) { }
+//    string get_boyname();
+//    void hello();
+//    void setGF(Girl *ds);
+//};
+//
+//
+//#endif //UNTITLED2_BOY_H
--- c++계절학기/2023.01.01/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Boy.h"
-#include "Girl.h"
-int main(){
-    Boy *b = new Boy("김철수");
-    Girl *g = new Girl("박영은");
-    b->setGF(g); g->setBF(b); // 남친/여친을 설정하는 함수
-    b->hello(); g->hello();
-}
