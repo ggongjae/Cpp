a7ef3d18d4a23f0979b678f8edc9243a311fdcef
ggongjae <cjdak0907@naver.com>
2023-01-06 10:54:34 +0900

    yongjae

---
+++ c++계절학기/2023.01.06/padak_comp.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//using namespace std;
+//class Padak{
+//public:
+//    Padak();
+//    void make_chicken(){
+//        pd -> make_chicken();
+//        cout << "파를 100그램 얹습니다." << endl;
+//    }
+//private:
+//    Kyochon *pd;
+//};
+//
+//Padak::Padak() {
+//    pd = new KyoChon();
+//}
