de4ccaa415e7dd33a50c9c600cc6892506af0a9e
ggongjae <cjdak0907@naver.com>
2023-01-12 00:59:43 +0900

    yongjae

--- c++계절학기/과제/과제5.cpp
+++ c++계절학기/과제/과제5.cpp
@@ -7,18 +7,18 @@
 //    int num;
 //    char name[10];
 //};
-//class Lscore : public virtual Student {
+//class Lscore {
 //protected:
 //    int kor;
 //    int eng;
 //};
-//class Nscore : public virtual Student {
+//class Nscore{
 //protected:
 //    int math;
 //    int sci;
 //};
 //
-//class Totscore : public Lscore, public Nscore {
+//class Totscore : public Lscore, Nscore,Student {
 //public:
 //    Totscore(int n, const char* na, int ko, int en, int mat, int sc) {
 //        num = n;
@@ -31,10 +31,10 @@
 //    Totscore operator +(int k) {
 //        return Totscore(num, name, kor + k, eng + k, math + k, sci + k);
 //    }
-//    friend void Print(Totscore *stu);
+//    friend void Print(Totscore stu[]);
 //};
 //
-//void Print(Totscore *stu) {
+//void Print(Totscore stu[]) {
 //    for (int i = 0; i < MAX; i++) {
 //        cout << " " << stu[i].num << " " << stu[i].name << " "
 //             << stu[i].kor << " " << stu[i].eng << " "
@@ -43,11 +43,11 @@
 //}
 //int main() {
 //    Totscore st[MAX] = {
-//            Totscore(101, "Kim", 85, 90, 80, 77),
-//            Totscore(102, "Lee", 90, 95, 89, 89),
-//            Totscore(103, "Park", 80, 70, 70, 55),
-//            Totscore(104, "Hong", 75, 80, 90, 85),
-//            Totscore(105, "Kang", 85, 90, 85, 85)
+//            Totscore(101, "kim", 85, 90, 80, 77),
+//            Totscore(102, "lee", 90, 95, 89, 89),
+//            Totscore(103, "park",80, 70, 70, 55),
+//            Totscore(104, "hong",75, 80, 90, 85),
+//            Totscore(105, "kang",85, 90, 85, 85)
 //    };
 //    for (int i = 0; i < MAX; i++) {
 //        st[i] = st[i] + 5;
